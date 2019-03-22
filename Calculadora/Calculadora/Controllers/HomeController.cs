cc67d05d4999895643d64863fdf2546a1c057056
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2019-03-22 20:19:46 +0000

    Adição do código do controller para processar os dados da calculadora

--- Calculadora/Calculadora/Controllers/HomeController.cs
+++ Calculadora/Calculadora/Controllers/HomeController.cs
@@ -4,14 +4,46 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace Calculadora.Controllers
-{
-    public class HomeController : Controller
-    {
-        // GET: Home
-        public ActionResult Index()
-        {
-            return View();
-        }
-    }
+namespace Calculadora.Controllers {
+   public class HomeController : Controller {
+
+      // GET: Home
+      public ActionResult Index() {
+         return View();
+      }
+
+
+      // POST: Home
+      [HttpPost]
+      public ActionResult Index(string visor, string bt) {
+         switch(bt) {
+            case "0":
+            case "1":
+            case "2":
+            case "3":
+            case "4":
+            case "5":
+            case "6":
+            case "7":
+            case "8":
+            case "9":
+               if(visor == "0") {
+                  visor = bt;
+               }
+               else {
+                  visor += bt;
+               }
+
+
+               break;
+         }
+
+         ViewBag.Resposta = visor;
+
+         return View();
+      }
+
+
+
+   }
 }
